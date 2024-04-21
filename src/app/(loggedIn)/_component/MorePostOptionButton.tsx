93c6975283011d7586350c7beed133fc0cd44225
hyeminem-Jo <konnimey@naver.com>
2024-04-21 21:41:31 +0900

    🎨 게시글 상세 (PostDetail) 작업중

--- src/app/(loggedIn)/_component/MorePostOptionButton.tsx
+++ src/app/(loggedIn)/_component/MorePostOptionButton.tsx
@@ -12,7 +12,6 @@ function MorePostOptionButton() {
       type="button"
       onClick={() => {
         openModal('postOption')
-        // alert('333')
       }}
     >
       <IcPostMoreOption />
